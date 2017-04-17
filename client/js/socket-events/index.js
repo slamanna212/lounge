a1ec4ba195390932dabc79a687162cb2b1a95165
Alistair McKinlay <github@10people.co.uk>
2017-04-17 16:43:59 +0100

    Create modules for lots of code

---
+++ client/js/socket-events/index.js
@@ -0,0 +1,16 @@
+"use strict";
+require("./auth");
+require("./change_password");
+require("./init");
+require("./join");
+require("./more");
+require("./msg");
+require("./names");
+require("./network");
+require("./nick");
+require("./part");
+require("./quit");
+require("./sync_sort");
+require("./toggle");
+require("./topic");
+require("./users");
---
+++ client/js/user-events/index.js
@@ -0,0 +1,3 @@
+"use strict";
+require("./message");
+require("./nick");
